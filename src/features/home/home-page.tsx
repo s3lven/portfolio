e6583bc0bf2ab1b97710f721e30da84488deff87
Eriz <erizjsub2@gmail.com>
2024-12-11 06:25:39 -0800

    feat: added skills section layout and content

--- src/features/home/home-page.tsx
+++ src/features/home/home-page.tsx
@@ -6,6 +6,21 @@ import { ChevronDown } from "lucide-react";
 import { Link } from "react-scroll";
 
 export const HomePage = () => {
+	const techStack = [
+		"typescript",
+		"react",
+		"nextjs",
+		"tailwind",
+		"figma",
+		"node",
+		"express",
+		"postgresql",
+		"git",
+		"github",
+	];
+
+	const techExplored = ["python", "mongodb", "java", "c", "c++", "docker"];
+
 	return (
 		<div className="w-full">
 			{/* Hero Section */}
@@ -62,49 +77,107 @@ export const HomePage = () => {
 				</Link>
 			</section>
 
+			{/* Career Section */}
 			<section id="career" className="bg-background border-y-2 border-white">
 				<div className="container mx-auto px-6 lg:px-0 py-16">
 					{/* Header */}
 					<div className="w-full h-16 flex justify-center items-center pb-16">
-						<h1 className="text-3xl font-bold">Career</h1>
+						<h1 className="text-3xl font-bold">Career 💼</h1>
 					</div>
 
 					{/* Mergen */}
-					<div className="flex gap-6 w-full">
-						<div className="">
-							<MergenIcon className={"size-40 bg-[#f222dd] rounded p-2"} />
+					<BlurFade delay={0.25} inView>
+						<div className="flex gap-6 w-full">
+							<div className="">
+								<MergenIcon className={"size-40 bg-[#f222dd] rounded p-2"} />
+							</div>
+							<div className="">
+								<h3 className="text-3xl font-bold leading-[1]">Mergen</h3>
+								<h4 className="text-primary mt-1.5 font-semibold">
+									Frontend Software Engineer Intern
+								</h4>
+								<h4 className="my-1 font-semibold">
+									Jul 2024 - <span>Now</span>
+								</h4>
+								<ul className="list-inside list-disc pt-2 ">
+									<li className="text-pretty">
+										Translated Figma into visually appealing, pixel-perfect web
+										pages using React, Tailwind CSS, and Typescript.
+									</li>
+									<li className="text-pretty">
+										Developed a text editor with SlateJS allowing students to
+										write and format essays. Integrated commenting functionality
+										using state management.
+									</li>
+									<li className="text-pretty">
+										Used Webflow to create a marketing website to attract new
+										users onto the platform. Used built-in CMS to create a job
+										board page for future Mergen applicants.
+									</li>
+									<li className="text-pretty">
+										Created a web scraping Python script using the Pandas and
+										Requests libraries to collect essay prompts and other data
+										from 328 colleges.
+									</li>
+								</ul>
+							</div>
 						</div>
-						<div className="">
-							<h3 className="text-3xl font-bold leading-[1]">Mergen</h3>
-							<h4 className="text-primary mt-1.5 font-semibold">
-								Frontend Software Engineer Intern
+					</BlurFade>
+				</div>
+			</section>
+
+			{/* Skills Section */}
+			<section
+				id="skills"
+				className="px-6 lg:px-0 container mx-auto py-20 place-content-center"
+			>
+				{/* Header */}
+				<div className="w-full h-16 flex justify-center items-center pb-16">
+					<h1 className="text-3xl font-bold">Skills 🛠️</h1>
+				</div>
+
+				<div className="h-auto flex justify-center gap-6 ">
+					<BlurFade delay={0.25} inView>
+						<div className="w-full p-6 rounded-md backdrop-blur-sm border-primary border">
+							<h3 className="text-xl font-semibold ">
+								The 'Full-Stack' Stack 🥞
+							</h3>
+							<h4 className="text-lg font-medium pt-4">
+								My go-to for whenver an idea comes to mind, often used for quick
+								prototyping!
 							</h4>
-							<h4 className="my-1 font-semibold">
-								Jul 2024 - <span>Now</span>
-							</h4>
-							<ul className="list-inside list-disc pt-2">
-								<li>
-									Translated Figma into visually appealing, pixel-perfect web
-									pages using React, Tailwind CSS, and Typescript.
-								</li>
-								<li>
-									Developed a text editor with SlateJS allowing students to
-									write and format essays. Integrated commenting functionality
-									using state management.
-								</li>
-								<li>
-									Used Webflow to create a marketing website to attract new
-									users onto the platform. Used built-in CMS to create a job
-									board page for future Mergen applicants.
-								</li>
-								<li>
-									Created a web scraping Python script using the Pandas and
-									Requests libraries to collect essay prompts and other data
-									from 328 colleges.
-								</li>
-							</ul>
+							<div className="flex flex-wrap gap-4 pt-8">
+								{techStack.map((tech) => (
+									<img
+										key={tech}
+										src={`src/assets/tech-icons/${tech}.svg`}
+										className="size-10"
+									/>
+								))}
+							</div>
 						</div>
-					</div>
+					</BlurFade>
+					<BlurFade delay={0.25} inView>
+						<div className="w-full p-6 rounded-md backdrop-blur-sm border-primary border">
+							<h3 className="text-xl font-semibold ">Tech I've Explored 🔎</h3>
+							<h4 className="text-lg font-medium pt-4">
+								"Jack of All Trades, Master of One" is how the saying goes.{" "}
+								<br />
+								<br />
+								Used in learning projects, allowing me to grasp their features
+								and applications.
+							</h4>
+							<div className="flex flex-wrap gap-4 pt-8">
+								{techExplored.map((tech) => (
+									<img
+										key={tech}
+										src={`src/assets/tech-icons/${tech}.svg`}
+										className="size-10"
+									/>
+								))}
+							</div>
+						</div>
+					</BlurFade>
 				</div>
 			</section>
 		</div>
